599a75e913fbc27621a1db838611ebcd8a58828d
c332030 <c332030@gmail.com>
2023-02-02 15:28:03 +0800

    feat: nexus init.bat

--- script/init.bat
+++ script/init.bat
@@ -2,16 +2,22 @@
 
 %utf8%
 
+echo.
 set /p NEXUS_APP=请输入 nexus 安装路径：
-
-set NEXUS="%NEXUS_APP%\bin\nexus"
-
 net stop nexus
-%NEXUS% /uninstall
 
-%NEXUS% /install
+set NEXUS_WORK="%NEXUS_APP%\..\sonatype-work\nexus3"
+set NEXUS_CACHE=F:\data\nexus
+
+call link-dir %NEXUS_CACHE%\blobs %NEXUS_WORK%\blobs
+call link-dir %NEXUS_CACHE%\cache %NEXUS_WORK%\cache
+call link-dir %NEXUS_CACHE%\log %NEXUS_WORK%\log
+call link-dir %NEXUS_CACHE%\tmp %NEXUS_WORK%\tmp
+
+set NEXUS_EXE="%NEXUS_APP%\bin\nexus"
+%NEXUS_EXE% /uninstall
+
+%NEXUS_EXE% /install
 net start nexus
-
-net status nexus
 
 pause >nul
