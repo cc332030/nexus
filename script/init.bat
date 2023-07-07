59fbd09ff7c259dcee3d498dcec6229498b9ffa9
c332030 <c332030@c332030.com>
2023-07-07 17:49:08 +0800

    feat: nexus script

--- script/init.bat
+++ script/init.bat
@@ -1,10 +1,9 @@
 @echo off
 
 echo.
-set /p NEXUS_APP=请输入 nexus 安装路径：
 net stop nexus
 
-set NEXUS_EXE="%NEXUS_APP%\bin\nexus"
+set NEXUS_EXE="%~dp0app\bin\nexus"
 %NEXUS_EXE% /uninstall
 
 %NEXUS_EXE% /install
---
+++ script/update.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set APP=app
+set APP_PATH=%~dp0
+set ZIP_PATH=%~dp0release.zip
+set URL=https://download.sonatype.com/nexus/3/latest-win64.zip
+
+net stop nexus
+
+curl -L -o %ZIP_PATH% %URL%
+
+rmdir /s /q %APP_PATH%%APP%
+
+7z x -y -o%APP_PATH% %ZIP_PATH%
+
+forfiles /p %APP_PATH% /m nexus* /c "cmd /c rename @file %APP%"
+
+net start nexus
+
+pause >nul
