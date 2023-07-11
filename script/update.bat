c9ad3a319eaa2c642b0c0184271a0474bbf01386
c332030 <c332030@c332030.com>
2023-07-11 19:22:41 +0800

    feat: 调整更新 nexue 脚本

--- script/update.bat
+++ script/update.bat
@@ -5,16 +5,19 @@ set APP_PATH=%~dp0
 set ZIP_PATH=%~dp0release.zip
 set URL=https://download.sonatype.com/nexus/3/latest-win64.zip
 
+set NEXUS_EXE="%APP_PATH%%APP%\bin\nexus"
+
 curl -L -o %ZIP_PATH% %URL%
 
 7z x -y -o%APP_PATH% %ZIP_PATH%
 
-net stop nexus
+%NEXUS_EXE% /uninstall
 
 rmdir /s /q %APP_PATH%%APP%
 
 forfiles /p %APP_PATH% /m nexus* /c "cmd /c rename @file %APP%"
 
+%NEXUS_EXE% /install
 net start nexus
 
 pause >nul
