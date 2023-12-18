ba067b4a566ebd5f9cee2c9569e044863f0c7f4b
c332030 <c332030@c332030.com>
2023-12-18 11:21:17 +0800

    feat: nexus update

--- script/update.bat
+++ script/update.bat
@@ -2,12 +2,19 @@
 
 set APP=app
 set APP_PATH=%~dp0
-set ZIP_PATH=%~dp0release.zip
+
+set ZIP_NAME=release.zip
+set TMP_SUFFER=.tmp
+
+set ZIP_PATH=%APP_PATH%%ZIP_NAME%
 set URL=https://download.sonatype.com/nexus/3/latest-win64.zip
 
 set NEXUS_EXE="%APP_PATH%%APP%\bin\nexus"
 
-curl -L -o %ZIP_PATH% %URL%
+curl -L -o %ZIP_PATH%%TMP_SUFFER% %URL%
+
+del /f /s /q %ZIP_NAME%
+rename %ZIP_NAME%%TMP_SUFFER% %ZIP_NAME%
 
 7z x -y -o%APP_PATH% %ZIP_PATH%
 
